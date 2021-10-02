55b85656a5e541fc87669dbc64c72f28b1cfb14f
Connor <mussonconnor@gmail.com>
2021-10-02 17:53:09 +0200

    Signup testing almost finished, just need to test the async function and have it all reviewed

---
+++ client/jammin/src/components/Signup/Signup.test.js
@@ -0,0 +1,28 @@
+import {render, screen} from '@testing-library/react';
+import Signup from './SignUp';
+
+
+describe('Signup component', ()=> {
+  
+  test('should match the snapshot', () => {
+    const {container} = render(<Signup
+                  setUserData={()=>{}}
+                  setIsSignedUp={()=>{}} />);
+    expect(container.firstChild).toMatchSnapshot();
+  })
+  
+  test('Should render the headings', () => {
+    render (<Signup
+                  setUserData={()=>{}}
+                  setIsSignedUp={()=>{}} />);
+    //title
+    screen.getByText(/Create your account/);
+    screen.getByPlaceholderText(/First name/);
+    screen.getByPlaceholderText(/Last name/);
+    screen.getByPlaceholderText(/Email/);
+    screen.getByPlaceholderText(/Password/);
+    screen.getByRole('button', { name: 'JOIN NOW' });
+  })
+
+})
+
