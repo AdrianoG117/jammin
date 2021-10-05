af0d6bb6810940b98cdcf4087a9654161857fb7c
AdrianoG117 <adrianogonzalezalberto@gmail.com>
2021-10-05 15:02:54 +0200

    resolved apiService refence issues

--- client/jammin/src/components/CreateJam/CreateJam.test.js
+++ client/jammin/src/components/CreateJam/CreateJam.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import CreateJam from "./CreateJam";
-import apiService from "../../ApiService";
+import apiService from "../../apiService/ApiService";
 import { userEvent } from "@testing-library/user-event";
 
 
