3026ccce9c90c75ed00f624bdfc793e9b2253b25
Connor <mussonconnor@gmail.com>
2021-10-05 12:02:09 +0200

    committing

--- client/jammin/src/components/Topbar/Topbar.tsx
+++ client/jammin/src/components/Topbar/Topbar.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import './topbar.css';
 import { useHistory } from 'react-router-dom';
 
-function Topbar({ setUserData, isSignedUp, setIsSignedUp }) {
+function Topbar ({ setUserData, isSignedUp, setIsSignedUp }) :FunctionComponent {
   const history = useHistory();
 
   return (
